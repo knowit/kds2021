8cc89bf97c0a4df1c34e23cfa6e32b365acc528f
Jan Henrik Gundelsby <jhg@knowit.no>
2016-10-21 10:46:34 +0200

    Default timer to 15 min, and possible to pass param timer

--- timer/javascripts/planning_poker_timer.js
+++ timer/javascripts/planning_poker_timer.js
@@ -2,9 +2,9 @@ var Counter = {
 
   init: function() {
     // http://kds.knowit.no/timer/?precision=1&time=600&restartAt=-120&colours[10]=ff0000&colours[0]=FF0000
-    // this.query = new Hash(window.location.search.toQueryParams());
+    this.query = new Hash(window.location.search.toQueryParams());
+    this.time = (this.query.get('time') || 900) * 1;  
     this.query = new Hash("?precision=1&time=600&restartAt=-120&colours%5B10%5D=ff0000&colours%5B0%5D=FF0000".toQueryParams());
-    this.time = (this.query.get('time') || 10) * 1;
     this.precision = (this.query.get('precision') || 2) * 1;
     this.initialColor = '#' + (this.query.get('initialColour') || '00ff00');
     this.restartAt = this.query.get('restartAt');
