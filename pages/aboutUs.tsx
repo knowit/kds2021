a62c8e9e3de2b7f4fa2781aedbb8ac1fc46802a6
571530 <truls.martinsen13@gmail.com>
2019-08-20 09:32:05 +0200

    Added about us page

---
+++ pages/aboutUs.tsx
@@ -0,0 +1,15 @@
+import Layout from "./components/Layout";
+import "../styling/styling.scss";
+
+export default function AboutUs() {
+  return (
+    <div className="aboutUs">
+      <Layout>
+        <div className="content">
+          <h1>About us</h1>
+          <p>More info will come at a later time</p>
+        </div>
+      </Layout>
+    </div>
+  );
+}
