1f94dbd0f8a0ed645bd97c7360d4ed7f834d5a85
571530 <truls.martinsen13@gmail.com>
2019-08-20 09:27:08 +0200

    Moved old files and timer into /static and updated link in layout component

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -15,7 +15,7 @@ const Layout = props => (
             <img id="location-img" src="../static/location.svg" />
             Bergen, Norway 17 - 18 January<hr />
           </h3>
-          <h4 className="header-subheadline">For the 2019-program, <a className="subheadline-link" href="/schedule"> see here </a></h4>
+          <h4 className="header-subheadline">For the 2019-program, <a className="subheadline-link" href="/static/old/index_2019.html"> see here </a></h4>
 
         </div>
       </div>
