c8c128c6705d520ab4a32e31da0b5f1e8880f4d4
571530 <truls.martinsen13@gmail.com>
2019-08-29 10:35:40 +0200

    moved config into a .env file, added dotenv dependency

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const withSass = require('@zeit/next-sass')
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
+
 module.exports = withSass({
-  /* config options here */
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+
+    return config;
+  }
 })
