93cffbf89ab2ed501d9aade1549f5366cf04c4cf
571530 <truls.martinsen13@gmail.com>
2019-09-19 14:48:28 +0200

    Moved images into sperate files

--- pages/components/FavouriteTalkButton.tsx
+++ pages/components/FavouriteTalkButton.tsx
@@ -13,7 +13,7 @@ class FavouriteTalkButton extends React.Component<IProps, any> {
     favouriteButtonClick = () => {
         this.setState({
             clicked: !this.state.clicked
-        })
+        });
         if (typeof localStorage != 'undefined' && !localStorage.getItem(this.props.talkId)) {
             localStorage.setItem(this.props.talkId, this.props.talkId)
         } else if (typeof localStorage != 'undefined') {
@@ -25,10 +25,10 @@ class FavouriteTalkButton extends React.Component<IProps, any> {
     }
 
     render() {
-            var icon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAAAmJLR0QA/4ePzL8AAAGNSURBVEgN7cG7ShUAAAbgr1SoB6ihCw5OdRpaDCvIoBy6kBBFRFHg4pDiUhRNkQRagVgPENHQAxQOYb5Dhi8Q2OARgjpeyOP5w0E5Hm9H26Lv81+NwwaNK5kxYdgRy44aNmFGyRcDDtlAjzkRERFlzzVo9EJZRETErHvW8VhEFI0YUxQRH3wUEUVjRhRFxCM1TlsU83o0WtLglikREVNua7CkUa95seikVT6LuKpai0kRk1pUuyZiVJX9FsUntQqmTSuoNSrK9llxVkSvtVq1WqtPRLsVl0XcUa+7Ii5ZcUzEU/XqF1GwYq+SGFevr6JkjypvRZxTjw4Rb6zSpiLGNdlKkwlRcUKN9yIGbeWliHfWOGhaVNywmZsqouiAdVxUEb9dsJHz5kXFFRt4IOKXNus5pSTivk08E1HSoVa7nyL6beG1iDmdqnWaE/HKlnYbFrGgy7IuCyKG7VKXhyIqnljSpyJiwDZ0K4sYMiSirNs2dZoVETHvuh1oMy3ihzN2qOCb7477C82a/ev+ABhYwYF0Ebe0AAAAAElFTkSuQmCC';
+            var icon = '../static/heart-filled.png';
             
             if (this.isFavorited(this.props.talkId)) {
-                icon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAAAmJLR0QA/4ePzL8AAAEYSURBVEjHY2AYBahAlqGT4RLDF4avDFcZJjJowsW1gLyrQNEvDBcZOhhkcGnPYfjO8B8J/mHoYmBmYGHoBrKQxb8xZGPTXoWiCAY3M2zBKl6Brt2a4S9WhbjgXwZLVAP2kqQdBPcgaxcj0X5ICIkiDHAkWTsI2iMM8CHLAG+EATpkGaCNMIATmERI1f6FgQM5GBeRbMB81Gi0YPhHkvZ/DGboSWk5SQYsxkzK0gxviNb+mkEKW27wItIb/xj8cOXHUqIMKMFXIrQS1N5MqFDpxat9CuFSiQlY+uDSPpGBkbiirRyr9g5SSscEht9omTeD1ALWH1j2wbT/YAglp4y2gCat9wx25Bbz2gyPGJ4xGFBSU8gD4XAHAKoautd2XZI2AAAAAElFTkSuQmCC';
+                icon = '../static/heart.png';
             }
             return (
                 <button onClick={() => this.favouriteButtonClick()} id="favourite-talk-button">
