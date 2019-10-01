cac1d4d85aa806e6bb95149abad268a16840e19b
571530 <truls.martinsen13@gmail.com>
2019-10-01 10:03:09 +0200

    Update build

---
+++ _next/static/chunks/styles.3b24d85e5862c7afd825.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"2jdu":function(n,o,c){},"4am6":function(n,o,c){},"6m6p":function(n,o,c){},HrOg:function(n,o,c){},Jwgg:function(n,o,c){},OQJa:function(n,o,c){},V4JW:function(n,o,c){},VNbw:function(n,o,c){},jkxm:function(n,o,c){},mLoh:function(n,o,c){},q1vP:function(n,o,c){},tGQ5:function(n,o,c){}}]);
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import Layout from "./components/Layout";
-import Practicalities from './practicalities';
-import CallForPresentations from "./callForPresentations";
-
-export default function Home() {
-  return <CallForPresentations />;
-}
