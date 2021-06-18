876be8d191c27c2a9971247b81c200e7a18be4ff
Eskil <eskilusken@gmail.com>
2021-06-18 12:39:34 +0200

    Changed header title

--- pages/aboutUs.tsx
+++ pages/aboutUs.tsx
@@ -1,25 +1,26 @@
 import { Layout } from "../components";
+import "../styling/callForPresentationsStyles.scss";
 
 const AboutUs = () => {
   return (
     <div className="aboutUs page">
       <Layout background={true}>
         <div className="document">
-          <h1>About</h1>
+          <h1>About Us</h1>
           <p>
-            <strong>Knowit Developer Summit (KDS)</strong> is Knowit's internal software development 
+            Knowit Developer Summit (KDS) is Knowit's internal software development 
             conference. The conference is put on by, and for, our passionate 
             developers at Knowit: we arrange it, we are its speakers, and we make 
             up its audience.
           </p>
           <p>
-            <strong>The ambition for KDS</strong> is that it will be held annually and that its 
+            The ambition for KDS is that it will be held annually and that its 
             location varies from year to year. The conference begins during Friday 
             around lunchtime and runs until Saturday afternoon. A conference dinner 
             is arranged on the Friday evening.
           </p>
           <p>
-            <strong>Hotels and conference facilities</strong> are carefully selected. We strive to 
+            Hotels and conference facilities are carefully selected. We strive to 
             ensure that accommodation is comfortable and that the conference is 
             located near a transportation hub such as an airport, train station or 
             bus terminal. The conference is always held in a city where Knowit 
@@ -27,12 +28,12 @@ const AboutUs = () => {
             must live nearby.
           </p>
           <p>
-            <strong>The conference committee</strong> consists of individuals from various Knowit 
+            The conference committee consists of individuals from various Knowit 
             companies. There are no rules regarding who can join the committee; new 
             members are welcome to get involved!
           </p>
           <p>
-            <strong>KDS is financed</strong> through cost price, and thus there is no profit target. 
+            KDS is financed through cost price, and thus there is no profit target. 
             The cost is shared equally between all participants, regardless of 
             where you live, whether you are a speaker, audience member, or a 
             member of the committee. Travel to and from the conference is handled 
