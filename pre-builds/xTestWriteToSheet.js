4657fd61cb8be6d2c83a32a74ded0aade60aebaa
Karl Oscar Furnes Warpe <oscarwarpe@gmail.com>
2021-06-28 13:31:08 +0200

    writing to multiple sheets and rows

--- pre-builds/xTestWriteToSheet.js
+++ pre-builds/xTestWriteToSheet.js
@@ -1,57 +1,64 @@
 import  { GoogleSpreadsheet } from 'google-spreadsheet'; 
 import dotenv from 'dotenv';
+import { truncateSync } from 'fs';
 
 const docId = '1B8sdJBfb3ercx_kPMfq0j5kCItxuSp6-7SXveW7UL7s';
 dotenv.config(); 
 
-async function spreadsheetWriter() {
+async function spreadsheetWriter(sheetDict, speakerDict, otherEventsDict) {
     const doc = new GoogleSpreadsheet(docId);
     await doc.useServiceAccountAuth({
         client_email: process.env.CLIENT_EMAIL,
         private_key: process.env.PRIVATE_KEY.toString().split('\\n').join('\n')   
     });
-    //doc.resetLocalCache();  
+
     await doc.loadInfo();
     await doc.updateProperties({title: 'WRITE TO SHEET TEST'});
-    
-    var sheet = doc.sheetsByTitle["Sheet1"]; 
-    // var header = "header"; 
+    var sheet = doc.sheetsByTitle["talks"]; 
 
     await sheet.setHeaderRow(Object.keys(sheetDict));
     await sheet.loadHeaderRow(); 
 
-    // append rows
-    var rows = await sheet.getRows(); 
     for (var i = 0; i < sheetDict["talkId"].length; i++) {
-        rows[i].talkId = sheetDict["talkId"][i];
-        rows[i].title = sheetDict["title"][i];
-        rows[i].language = sheetDict["language"][i];
-        rows[i].difficulty = sheetDict["difficulty"][i];
-        rows[i].type = sheetDict["type"][i];
-        rows[i].room = sheetDict["room"][i];
-        rows[i].day = sheetDict["day"][i];
-        rows[i].timeStart = sheetDict["timeStart"][i];
-        rows[i].timeEnd = sheetDict["timeEnd"][i];
-        rows[i].tags = sheetDict["tags"][i]; // .join(" | ");
-        await rows[i].save(); 
+        await sheet.addRow({
+            talkId: sheetDict["talkId"][i],
+            title: sheetDict["title"][i],
+            language: sheetDict["language"][i],
+            difficulty: sheetDict["difficulty"][i],
+            type: sheetDict["type"][i],
+            day: sheetDict["day"][i],
+            timeStart: sheetDict["timeStart"][i],
+            timeEnd: sheetDict["timeEnd"][i],
+            tags: sheetDict["tags"][i],
+        });
     }
 
-    //const larryRow = await sheet.addRow({ talkId: 'Larry Page', title: 'larry@google.com' }); 
+    sheet = doc.sheetsByTitle["speakers"]; 
+    await sheet.setHeaderRow(Object.keys(speakerDict));
+    await sheet.loadHeaderRow(); 
 
-    // append rows
-    //const larryRow = await sheet.addRow({ name: 'Larry Page', email: 'larry@google.com' });
-    //const moreRows = await sheet.addRows([
-    //{ name: 'Sergey Brin', email: 'sergey@google.com' },
-    //{ name: 'Eric Schmidt', email: 'eric@google.com' },
-    //]);
-    
-   
+    for (var i = 0; i < speakerDict["talkId"].length; i++) {
+        await sheet.addRow({
+            talkId: speakerDict["talkId"][i],
+            name: speakerDict["name"][i],
+            info: speakerDict["info"][i],
+        });
+    }
 
-    //const sheet = await doc.addSheet({ headerValues: ['name', 'email'] });
-    
-    //await sheet.addRow({ name: 'Larry Page', email: 'larry@google.com' });
-    //rows[1] = 'sergey@abc.xyz'; // update a value
-    //await rows[1].save(); // save updates
+    sheet = doc.sheetsByTitle["otherEvents"]; 
+    await sheet.setHeaderRow(Object.keys(otherEventsDict));
+    await sheet.loadHeaderRow(); 
+
+    for (var i = 0; i < otherEventsDict["type"].length; i++) {
+        console.log(i);
+        await sheet.addRow({
+            type: otherEventsDict["type"][i],
+            day: otherEventsDict["day"][i],
+            timeStart: otherEventsDict["timeStart"][i],
+            timeEnd: otherEventsDict["timeEnd"][i],
+        });
+    }
+
 }
 
 const sheetDict = {
@@ -93,26 +100,26 @@ const sheetDict = {
 }
 
 const speakerDict = {
-    name: [
+    "name": [
       'Fredrik Høgberg',
       'Marcus Grenangen',
       'Maja Jaakson',
       'Lewandowski'
     ],
-    info: [
+    "info": [
       '.NET Developer, Certified Lead Developer',
       'Marcus is a long time software engineer that has worked around the world in various technical roles.\\n\\nMarcus has worked at Microsoft, written core infrastructures in AAA games such as Battlefield and Starwars as well as worked on open source technologies to name a few things.\\n\\nMarcus has a deep passion for software and open source and tries his best to try to make the world a little bit better, through software and services. ',
       'Maja is a front-end developer by day and a sophist by night. She enjoys candlelight pair programming, long walks to the powerlifting gym, and annoying innocent bystanders with questions about what makes something a programming language.',
       'Lewandowski is from Poland, and he speaks Polish. He is here today to speak about nail polish. Also he is decent at football.'
     ],
-    talkId: [ '1 | 5', '3', '2 | 4', '2|3' ]
+    "talkId": [ '1 | 5', '3', '2 | 4', '2|3' ]
 }
 
-const otherEventDict = {
-    type: [ 'Lunch', 'Lunch', 'Welcome' ],
-    day: [ 'fredag', 'lørdag', 'fredag' ],
-    timeStart: [ '1130', '1130', '1000' ],
-    timeEnd: [ '1230', '1230', '1030' ]
+const otherEventsDict = {
+    "type": [ 'Lunch', 'Lunch', 'Welcome' ],
+    "day": [ 'fredag', 'lørdag', 'fredag' ],
+    "timeStart": [ '1130', '1130', '1000' ],
+    "timeEnd": [ '1230', '1230', '1030' ]
 }
 
 spreadsheetWriter(); 
