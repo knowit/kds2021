1612098f910edc31891f31095c7443d220e606fd
Karl Oscar Furnes Warpe <oscarwarpe@gmail.com>
2021-07-05 17:13:55 +0200

    install npm react transition group package

--- components/ScheduleTitle.tsx
+++ components/ScheduleTitle.tsx
@@ -1,6 +1,10 @@
 import { NONAME } from 'dns';
-import React from 'react'
-import LinesEllipsis from 'react-lines-ellipsis'
+import React from 'react';
+import LinesEllipsis from 'react-lines-ellipsis'; 
+import { Transition } from 'react-transition-group'; 
+
+
+
 
 interface props {
     title: string;
@@ -8,31 +12,40 @@ interface props {
 }
 
 const ScheduleTitle = ({title, clamped}:props) => {
+    
+    
+    const [inProp, setInProp] = React.useState(false); 
+
+
     let [over, setOver] = React.useState(false);
 
     let visibility = {
         visibility: 'hidden'
     }
 
-return (
-    <div    onMouseOver={()=>setOver(true)} 
-    onMouseOut={()=>setOver(false)}>
-        <div >
-<LinesEllipsis
-style={{display: over? 'none' : 'block'}}
-className='title'
-text={title}
-maxLine='3'
-ellipsis='...'
-trimRight
-basedOn='letters'
-isClamped='false'
-/>
-</div>
-<h1 className="title" style={{display: over? 'block' : 'none'}}>
-    {title}
-</h1>
-</div>
-);
+    return (
+        <div 
+            onMouseOver={()=>setOver(true)} 
+            onMouseOut={()=>setOver(false)}>
+
+            <div>
+                <LinesEllipsis
+                    style={{display: over? 'none' : 'block'}}
+                    className='title'
+                    text={title}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    isClamped='false'
+                />
+            </div>
+
+            <h1 className="title" style={{display: over? 'block' : 'none'}}>
+                {title}
+            </h1>
+
+        </div>
+    );
 }
 export default ScheduleTitle
