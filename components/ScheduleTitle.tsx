a88e9ac0b6bf51fe1003df55b76e712e61f03b9e
Karl Oscar Furnes Warpe <oscarwarpe@gmail.com>
2021-07-06 10:13:12 +0200

    update ScheduleTitle.tsx

--- components/ScheduleTitle.tsx
+++ components/ScheduleTitle.tsx
@@ -37,7 +37,7 @@ const ScheduleTitle = ({title, clamped}:props) => {
                     ellipsis='...'
                     trimRight
                     basedOn='letters'
-                    isClamped='false'
+                    isClamped='true'
                 />
             </div>
 
