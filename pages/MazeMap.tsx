7ced35316af0c6291cfd99eba739d664f12b9871
Karl Oscar Furnes Warpe <oscarwarpe@gmail.com>
2021-07-08 12:18:44 +0200

    update MazeMap url

--- pages/MazeMap.tsx
+++ pages/MazeMap.tsx
@@ -23,7 +23,7 @@ const MazeMap = () => {
           </div>
           <iframe
             id="mazemap"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d8140.445489541563!2d18.0595668!3d59.3310944!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdec7d747ae5962d4!2sHotel%20Continental!5e0!3m2!1sno!2sno!4v1614064915940!5m2!1sno!2sno"
+            src="https://www.use.mazemap.com/"
           />
         </div>
       </Layout>
