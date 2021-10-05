85e91196cb4f80b572a605e6117f0fee0da6cbc1
Erik Lund <erik.lund@knowit.no>
2021-10-05 17:11:51 +0200

    Rewrite call for presentations text
    Indicate that registration is now closed, but with consideration for applying to a waiting list.

--- pages/callForPresentations.tsx
+++ pages/callForPresentations.tsx
@@ -24,38 +24,17 @@ const CallForPresentations = () => {
               &nbsp;page.
             </p>
             <p>
-              While the submision deadline was on <strong>1st of September 2021</strong>, we are 
-              currently still accepting new presentations. You may add just a brief 
-              abstract and complete it later; the most important thing right now is 
-              to get all the presentations registered. You may submit more than one 
-              presentation if you wish. You can also hold a joint presentation; maybe 
-              you and a colleague are working on something interesting and would like 
-              to present it together? The length of a presentation can be 10, 30, 60 
-              or 90 minutes. You decide! Click the button at the bottom of the page to register.
+              The deadline for speaker registration was on <strong>the 1st September 2021</strong> and registration is now closed.
             </p>
             <p>
-              If you have any questions feel free to contact us at&nbsp;
+              If you wish to be registered to the waiting list for late speakers or have any questions feel free to contact us at&nbsp;
               <span>
                 <a className="link" href="mailto:kds@knowit.no">
                   kds@knowit.no
                 </a>
               </span>
             </p>
-            <p>
-              The subject for a talk can vary widely, you can check out last
-              years program&nbsp;
-              <Link href="https://kds-archive.knowit.no/schedule">
-                <a className="link">here</a>
-              </Link>
-              &nbsp;for inspiration. If that is not inspiring enough, here are a
-              few more ideas:
-            </p>
-            <img className="wordcloud" src="../static/images/WordCloud2021.png" alt="Wordcloud"></img>
           </div>
-          <RegisterButton
-            url="https://docs.google.com/forms/d/1-0n_08zyFXJGEHRJ2RM-sFxZZYepEbhxwQigECMXqfs"
-            name="Register your presentation"
-          ></RegisterButton>
         </div>
       </Layout>
     </div>
