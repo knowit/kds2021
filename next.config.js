b957be6167bd35eac9031866eae1e81be3117109
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-10-09 13:44:20 +0200

    test new exportpathmap

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
-module.exports = {}
+module.exports = {
+    exportPathMap: async function (defaultPathMap,  {dev, dir, outdir, distdir, buildId}) {
+        return {
+            '/': {page: '/'},
+            '/CallForPresentations': {page: '/callForPresentations'},
+            '/ParticipantRegistration': {page: '/participantRegistration'},
+            '/CodeOfConduct': {page: '/codeOfConduct'},
+            '/Location': {page: '/location'},
+            '/Practicalities': {page: '/practicalities'},
+            '/AboutKds': {page: '/aboutKds'},
+            '/Schedule': {page: '/schedule'},
+            '/TalksAndSpeakers': {page: '/talksAndSpeakers'},
+            '/Logo': {page: '/logo'},
+            '/Stats': {page: '/stats'}
+        }
+    }
+}
